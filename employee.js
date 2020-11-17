efd0a5b8602c2de6f9f09278f7980aa42a11873d
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-17 20:33:03 +0530

    [Akash]ADDED: Calculating Wages for a Month assuming 20 Working Days in a Month

--- employee.js
+++ employee.js
@@ -10,28 +10,36 @@
 }
 
 // UC2 - Calculating Employee Wage
-{
-    const IS_PART_TIME = 1;
-    const IS_FULL_TIME = 2;
-    const PART_TIME_HOURS = 4;
-    const FULL_TIME_HOURS = 8;
-    const WAGE_PER_HOUR = 20;
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    let empHrs = getWorkingHours(empCheck);
-    let empWage = empHrs * WAGE_PER_HOUR;
-    console.log("Employee Wage: " + empWage);
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+let empHrs = getWorkingHours(empCheck);
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Employee Wage: " + empWage);
 
-    // UC3 - Function for daily working Hours
-    function getWorkingHours(empCheck) {
-        switch (empCheck) {
-            case IS_PART_TIME:
-                return PART_TIME_HOURS;
-                break;
-            case IS_FULL_TIME:
-                return FULL_TIME_HOURS;
-                break;
-            default:
-                return 0;
-        }
+// UC3 - Function for daily working Hours
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+            break;
+        default:
+            return 0;
     }
+ }
+
+// UC4 - Calculating wages for a month
+const NUM_OF_WORKING_DAYS = 20;
+empHrs = 0;
+for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
 }
+empWage = empHrs * WAGE_PER_HOUR;
+console.log("Total Hrs: " + empHrs + " Employee Wage: " + empWage);
