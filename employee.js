3b211f0bef7550fef47bae927cc02e50150e0595
akashgupta3852 <akashgupta3852@gmail.com>
2020-11-17 21:24:23 +0530

    [Akash]ADDED: Storing the daily wage along with the total wage in a array

--- employee.js
+++ employee.js
@@ -45,14 +45,38 @@ empWage = empHrs * WAGE_PER_HOUR;
 console.log("UC4 - Total Hrs: " + empHrs + " Employee Wage: " + empWage);
 
 // UC5 - Calculate Wages till a condition of total working hours of 160 or max days of 20 is reached for a month
-const MAX_HRS_IN_MONTH = 100;
-let totalEmpHrs = 0;
-let totalWorkingDays = 0;
-while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
-    totalWorkingDays++;
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
+{
+    const MAX_HRS_IN_MONTH = 100;
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
+    }
+    empWage = totalEmpHrs * WAGE_PER_HOUR;
+    console.log("UC5 - Total Days: " + totalWorkingDays +
+        " Total Hrs: " + totalEmpHrs + " Employee Wage: " + empWage);
 }
-empWage = totalEmpHrs * WAGE_PER_HOUR;
-console.log("UC5 - Total Days: " + totalWorkingDays +
-    " Total Hrs: " + totalEmpHrs + " Employee Wage: " + empWage);   
+
+//UC6 - Storing the Daily Wage along with the Total Wage
+{
+    function calcDailyWage(empHrs) {
+        return empHrs * WAGE_PER_HOUR;
+    }
+
+    const MAX_HRS_IN_MONTH = 160;
+    let totalEmpHrs = 0;
+    let totalWorkingDays = 0;
+    let empDailyWageArr = new Array();
+    while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        totalWorkingDays++;
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
+        empDailyWageArr.push(calcDailyWage(totalEmpHrs));
+    }
+    let empWage = calcDailyWage(totalEmpHrs);
+    console.log("UC6 - Total Days: " + totalWorkingDays +
+        " Total Hrs: " + totalEmpHrs + " Employee Wage: " + empWage);
+    console.log("Array: " + empDailyWageArr);
+}
